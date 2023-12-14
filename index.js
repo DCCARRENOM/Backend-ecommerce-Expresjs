e47654f2ade4923f3e279079fde55f0a417062de
Daniel Carreño <danielcarreno0102@gmail.com>
2023-12-14 15:33:11 -0500

    Añadidos primero get de prueba con parametros

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
-console.log("My App");
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.get('/', (req, res) => {
+  res.send('Hola Mundo en Express');
+});
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+});
+
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Producto 1',
+      price: 1000,
+    },
+    {
+      name: 'Producto 2',
+      price: 2000,
+    },
+  ]);
+});
+
+app.get('/products/:id',(req,res) =>{
+  const {id} = req.params
+  res.json({
+    id,
+    name: "Producto 2",
+    price: 2000
+  });
+})
+
+app.get('/categories/:categoryId/products/:productId', (req,res) =>{
+  const {categoryId, productId} = req.params
+  res.json({
+    categoryId,
+    productId
+  })
+})
+
+app.listen(port, () => {
+  console.log('Mi puerto ' + port);
+});
