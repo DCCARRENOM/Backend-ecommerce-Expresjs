ff753919a71743bc4a575de9829eab23918c7428
Daniel Carreño <danielcarreno0102@gmail.com>
2023-12-19 22:35:47 -0500

    Agregado asincronismo al servicio de productos con promesas y creacion de los metodos create,update y delete de productos

--- services/product.service.js
+++ services/product.service.js
@@ -1,4 +1,4 @@
-const {faker} = require('@faker-js/faker')
+const { faker } = require('@faker-js/faker');
 
 class ProductsService {
   constructor() {
@@ -18,19 +18,48 @@ class ProductsService {
     }
   }
 
-  create() {}
+  async create(data) {
+    const newProduct = {
+      id: faker.string.uuid(),
+      ...data,
+    };
+    this.products.push(newProduct);
+    return newProduct;
+  }
 
   find() {
-    return this.products;
+    return new Promise((resolve, reject) =>{
+      setTimeout(() =>{
+        resolve(this.products);
+      }, 5000);
+    })
   }
 
-  findOne(id) {
-    return this.products.find(item => item.id === id);
+  async findOne(id) {
+    return this.products.find((item) => item.id === id);
   }
 
-  update() {}
+  async update(id, changes) {
+    const index = this.products.findIndex((item) => item.id === id);
+    if (index === -1) {
+      throw new Error('Product not found');
+    }
+    const product = this.products[index];
+    this.products[index] = {
+      ...product,
+      ...changes
+    };
+    return this.products[index];
+  }
 
-  delete() {}
+  async delete(id) {
+    const index = this.products.findIndex((item) => item.id === id);
+    if (index === -1) {
+      throw new Error('Product not found');
+    }
+    this.products.splice(index, 1);
+    return { id };
+  }
 }
 
 module.exports = ProductsService;
